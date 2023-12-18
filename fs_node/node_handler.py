9a3c935e4c15ebda66594fc5e334e2a34de01dae
Marjers <js.pdo.silv@gmail.com>
2023-12-18 14:43:57 +0000

    Final

--- fs_node/node_handler.py
+++ fs_node/node_handler.py
@@ -1,68 +1,33 @@
 import socket
 import json
-import sys
+
 from datetime import datetime
 import os
+from common import utilities
 
+nodes_info = {} 
 
-from ..common import utilities
-
-
-# Função para gerar a lista de informações dos arquivos para registro
 def get_file_info(directory):
-    
-#    Gera a lista de informações dos arquivos para registro.
-#
-#    Args:
-#        directory (str): O diretório contendo os arquivos a serem registrados.
-#
-#    Returns:
-#        list: Uma lista de dicionários contendo informações sobre cada arquivo.
-    
     files = []
     for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            filepath = os.path.join(directory, filename)
-            blocks = get_file_blocks(filepath)
+        filepath = os.path.join(directory, filename)
+        if os.path.isfile(filepath):
+            blocks = utilities.get_file_blocks(filepath)
             files.append({
                 "filename": filename,
                 "filesize": os.path.getsize(filepath),
-                "blocks": [{"index": i, "hash": block_hash} for i, (block, block_hash) in enumerate(blocks)]
+                "blocks": [{"index": i, "hash": block_hash} for i, (block_hash) in enumerate(blocks)]
             })
     return files
 
-# Função para enviar uma requisição para o FS_Tracker e receber a resposta
 def send_request_to_tracker(tracker_address, request_data):
-    
-#    Envia uma requisição para o FS_Tracker e recebe a resposta.
-#
-#    Args:
-#        tracker_address (tuple): Uma tupla contendo o endereço IP e a porta do FS_Tracker.
-#        request_data (dict): Um dicionário contendo os dados da requisição.
-#
-#    Returns:
-#        dict: Um dicionário com a resposta do FS_Tracker.
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(tracker_address)
         s.sendall(json.dumps(request_data).encode('utf-8'))
         response = s.recv(1024)
     return json.loads(response.decode('utf-8'))
 
-# Função para registrar o nó no FS_Tracker com a lista de arquivos
 def register_node(tracker_address, node_name, node_address, directory):
-
-#    Registra o nó no FS_Tracker com a lista de arquivos.
-#
-#    Args:
-#        tracker_address (tuple): Uma tupla contendo o endereço IP e a porta do FS_Tracker.
-#        node_id (str): O ID único do nó.
-#        node_address (str): O endereço do nó.
-#        directory (str): O diretório contendo os arquivos do nó.
-#
-#    Returns:
-#        dict: Um dicionário com a resposta do FS_Tracker.
-
     files = get_file_info(directory)
     request_data = {
         "action": "register",
@@ -73,20 +38,7 @@ def register_node(tracker_address, node_name, node_address, directory):
     }
     return send_request_to_tracker(tracker_address, request_data)
 
-# Função para atualizar as informações do nó no FS_Tracker
 def update_node_info(tracker_address, node_name, node_address, directory):
-
-#    Atualiza as informações do nó no FS_Tracker.
-#
-#    Args:
-#        tracker_address (tuple): Uma tupla contendo o endereço IP e a porta do FS_Tracker.
-#        node_id (str): O ID único do nó.
-#        node_address (str): O endereço do nó.
-#        directory (str): O diretório contendo os arquivos do nó.
-#
-#    Returns:
-#        dict: Um dicionário com a resposta do FS_Tracker.
-
     files = get_file_info(directory)
     request_data = {
         "action": "update",
@@ -97,19 +49,7 @@ def update_node_info(tracker_address, node_name, node_address, directory):
     }
     return send_request_to_tracker(tracker_address, request_data)
 
-# Função para consultar a localização de um arquivo no FS_Tracker
 def query_file_location(tracker_address, node_name, file_name):
-    
-#    Consulta a localização de um arquivo no FS_Tracker.
-#
-#    Args:
-#        tracker_address (tuple): Uma tupla contendo o endereço IP e a porta do FS_Tracker.
-#        node_id (str): O ID único do nó.
-#        file_name (str): O nome do arquivo a ser consultado.
-#
-#    Returns:
-#        dict: Um dicionário com a resposta do FS_Tracker.
-
     request_data = {
         "action": "query",
         "node_name": node_name,
@@ -117,3 +57,46 @@ def query_file_location(tracker_address, node_name, file_name):
         "timestamp": datetime.now().isoformat()
     }
     return send_request_to_tracker(tracker_address, request_data)
+
+def register_files_at_tracker(tracker_address, node_name, node_address, shared_directory):
+    files_info = get_file_info(shared_directory)
+    request_data = {
+        "action": "register",
+        "node_name": node_name,
+        "node_address": node_address,
+        "files": files_info,
+        "timestamp": datetime.now().isoformat()
+    }
+    response = send_request_to_tracker(tracker_address, request_data)
+    return response
+    
+
+def find_file_location(tracker_address, node_name, file_name):
+    request_data = {
+        "action": "query",
+        "node_name": node_name,
+        "filename": file_name,
+        "timestamp": datetime.now().isoformat()
+    }
+    response = send_request_to_tracker(tracker_address, request_data)
+    return response
+
+def process_registration_request(request_data):
+    node_name = request_data["node_name"]
+    node_address = request_data["node_address"]
+    files = request_data["files"]
+    
+    nodes_info[node_name] = {
+        "address": node_address,
+        "files": files
+    }
+    
+    
+def process_query_request(request_data):
+    file_name = request_data["filename"]
+    nodes_with_file = [
+        {"name": node, "address": info["address"]} 
+        for node, info in nodes_info.items() 
+        if file_name in [f["filename"] for f in info["files"]]
+    ]
+    return {"nodes": nodes_with_file}
--- fs_node/udp_client.py
+++ fs_node/udp_client.py
@@ -3,6 +3,12 @@ import json
 import os
 import hashlib
 import time
+from node_handler import query_file_location
+from node_handler import send_request_to_tracker
+
+import random
+import threading
+
 
 # Configurações globais
 BLOCK_SIZE = 4096  # Tamanho do bloco em bytes (aqui, 4KB)
@@ -41,71 +47,125 @@ def send_udp_request(server_name, server_address, request_data):
         except Exception as e:
             return None, str(e)
 
-def request_block(file_name, block_id, server_name, server_address):
-    # Solicita um bloco específico do arquivo ao servidor, com retransmissões se necessário.
-    #
-    # Args:
-    #     file_name (str): Nome do arquivo a ser solicitado.
-    #     block_id (int): ID do bloco do arquivo a ser solicitado.
-    #     server_address (tuple): Endereço do servidor (host, port).
-    #
-    # Returns:
-    #     bytes: Dados do bloco solicitado ou None se falhar.
-    
-    #request_data = {'action': 'request_block', 'file_name': file_name, 'block_id': block_id}
-    #print(f"Solicitando Bloco {block_id} do Ficheiro {file_name}")
-
+def request_block(blocks, block_index, file_name, source, expected_checksum):
+    server_address = (source['address'], source['port'])
     retries = 0
     while retries < MAX_RETRIES:
-        # Prepara a solicitação de bloco.
-        request_data = {'action': 'request_block', 'file_name': file_name, 'block_id': block_id}
-        start_time = time.time()  # Iniciar o cronômetro antes do envio da solicitação
-        response, error = send_udp_request(server_name, server_address, request_data)
-        end_time = time.time()
-        # Calcular tempo de resposta e taxa de transferência
-        response_time = end_time - start_time
-        block_size = len(response['data']) if response and 'data' in response else 0
-        transfer_rate = block_size / response_time if response_time > 0 else 0
-        
+        request_data = {'action': 'request_block', 'file_name': file_name, 'block_id': block_index}
+        response, error = send_udp_request(server_address, request_data)
+
         if response and 'data' in response:
-            # Decodifica e verifica a integridade do bloco.
-            block_data = response['data'].encode()
+            block_data = response['data'].encode('latin1')
             block_hash = calculate_block_hash(block_data)
 
-            if block_hash == response.get('checksum'):
-                return block_data
+            if block_hash == expected_checksum:
+                blocks[block_index] = block_data
+                return
             else:
-                print(f"Erro de integridade no bloco {block_id}. Tentando novamente...")
-        else:
-            print(f"Erro ao solicitar o bloco {block_id}: {error}. Tentando novamente...")
+                print(f"Erro de integridade no bloco {block_index}. Tentando novamente...")
 
         retries += 1
         time.sleep(1)
 
-    print(f"Não foi possível obter o bloco {block_id} após {MAX_RETRIES} tentativas.")
-    return None
+    print(f"Não foi possível obter o bloco {block_index} após {MAX_RETRIES} tentativas.")
+    blocks[block_index] = None
 
 def request_file(file_name, server_name, server_address, output_file):
-    # Solicita um arquivo inteiro, bloco por bloco, do servidor e o monta localmente.
-    #
-    # Args:
-    #     file_name (str): Nome do arquivo a ser solicitado.
-    #     server_address (tuple): Endereço do servidor (host, port).
-    #     output_file (str): Caminho do arquivo onde os dados serão salvos.
-
-    # Determina o número de blocos necessários para o arquivo.
-    file_size = os.path.getsize(file_name)
-    num_blocks = (file_size + BLOCK_SIZE - 1) // BLOCK_SIZE
+    file_blocks_info = query_file_location(server_address, server_name, file_name)
+    if 'blocks' not in file_blocks_info:
+        print("Erro ao obter informações do arquivo do FS_Tracker.")
+        return
 
     with open(output_file, 'wb') as file:
-        for block_id in range(num_blocks):
-            # Solicita cada bloco e escreve no arquivo de saída.
+        for block_info in file_blocks_info['blocks']:
+            block_id = block_info['index']
+            expected_checksum = block_info['hash']
             block_data = request_block(file_name, block_id, server_name, server_address)
             if block_data is None:
                 print(f"Falha ao baixar o bloco {block_id} do arquivo {file_name}.")
                 return
             file.write(block_data)
+            
+            
+def select_sources(file_blocks_info):
+    # Esta função pode ser simples (escolha aleatória) ou complexa (baseada em critérios específicos)
+    selected_sources = {}
+    for block in file_blocks_info['blocks']:
+        selected_node = random.choice(block['available_nodes'])
+        selected_sources[block['index']] = selected_node
+    return selected_sources            
 
+def get_alternative_source(tracker_address, file_name, block_id, excluded_source):
+    """
+    Obtém uma fonte alternativa para um bloco específico de um arquivo.
+
+    Args:
+        tracker_address (tuple): Endereço do FS_Tracker (host, port).
+        file_name (str): Nome do arquivo que está sendo baixado.
+        block_id (int): ID do bloco que precisa de uma fonte alternativa.
+        excluded_source (dict): Fonte a ser excluída da nova busca.
+
+    Returns:
+        dict or None: Retorna uma nova fonte se disponível, caso contrário None.
+    """
+    request_data = {
+        "action": "query_alternative_source",
+        "filename": file_name,
+        "block_id": block_id,
+        "excluded_source": excluded_source
+    }
+
+    response = send_request_to_tracker(tracker_address, request_data)
+    if response and 'status' in response and response['status'] == 'success':
+        return response.get('new_source')
+    else:
+        print("Não foi possível encontrar uma fonte alternativa.")
+        return None
+
+def download_file(file_name, file_blocks_info, output_file):
+    selected_sources = select_sources(file_blocks_info)
+    blocks = {}
+    threads = []
+
+    for block_info in file_blocks_info['blocks']:
+        block_index = block_info['index']
+        expected_checksum = block_info['hash']
+        source = selected_sources[block_index]
+
+        thread = threading.Thread(target=request_block, args=(blocks, block_index, file_name, source, expected_checksum))
+        threads.append(thread)
+        thread.start()
+    
+    for thread in threads:
+        thread.join()
+
+    with open(output_file, 'wb') as file:
+        for i in range(len(file_blocks_info['blocks'])):
+            if blocks[i]:
+                file.write(blocks[i])
+            else:
+                print(f"Falha ao reconstruir o arquivo: Bloco {i} está faltando.")
+
+def request_block(blocks, block_index, file_name, source, expected_checksum):
+    retries = 0
+    while retries < MAX_RETRIES:
+        server_address = (source['address'], source['port'])
+        block_data = send_udp_request(file_name, block_index, server_address, expected_checksum)
+
+        if block_data:
+            blocks[block_index] = block_data
+            return
+        else:
+            print(f"Retransmissão para o bloco {block_index} falhou. Tentando outra fonte...")
+            # Lógica para escolher outra fonte, se disponível.
+            new_source = get_alternative_source(file_name, block_index)
+            if new_source:
+                source = new_source
+            else:
+                print(f"Não há fontes alternativas disponíveis para o bloco {block_index}.")
+                break
+
+        retries += 1
 
 
 # Exemplo de uso
